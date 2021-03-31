d251d4a2d3229126b622fe2335a4176f618c64c7
JonM <jonmisner86@gmail.com>
2021-03-31 13:00:25 -0500

    fully working version

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".nav-menu");
+const inventoryMenu = document.querySelector("#inventory-menu");
+const inventory = document.querySelector("#inventory-toggle")
 
 menu.addEventListener("click", () => {
    menu.classList.toggle("is-active");
    menuLinks.classList.toggle("active");
+})
+
+inventory.addEventListener("click", () => {
+   inventory.classList.toggle("inv-is-active");
+   inventoryMenu.classList.toggle("inv-active");
 })
